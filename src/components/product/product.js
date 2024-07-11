6f8798a55d9d1579ae5d7a48143448eb5e29bdb3
Seshu Vuggina <seshuggina@gmail.com>
2024-07-11 22:40:34 +0530

    Products page filteration done

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,51 +1,45 @@
 import React, { memo, useEffect } from "react";
 import { Link } from "react-router-dom";
-// reactstrap components
 import { Badge, Card, CardBody } from "reactstrap";
-import AOS from "aos";
 import "./product.scss";
 import ImageLoad from "../Image/ImageLoad";
 
 export const Product = memo(({ product, thumbnailColor }) => {
   useEffect(() => {
-    AOS.init();
+    // AOS.init();
   }, []);
 
   return (
-    <>
-      {/* <Col lg="3 mb-4 product-thumbnail"> */}
-      <Card className="card-lift--hover shadow border-0 card-border product-thumbnail" data-aos="zoom-in">
-        <CardBody className="py-3">
-          <ImageLoad
-            imageName={product.productImage}
-            altTxt={product.impurityName}
-          />
-          <h6 className="text-primary text-uppercase">{product.parentAPI}</h6>
-          <p className="description mt-3">{product.impurityName}</p>
-          <div>
-            <Badge color={thumbnailColor} pill className="mr-1">
-              {product.casNo}
-            </Badge>
-            <Badge color={thumbnailColor} pill className="mr-1">
-              {product.leadTime}
-            </Badge>
-            <Badge color={thumbnailColor} pill className="mr-1">
-              {product.readyStock}
-            </Badge>
-          </div>
-          <Link
-            type="button"
-            className="mt-4 btn btn-primary"
-            color="primary"
-            as={Link}
-            outline
-            to={`/productDetails/${product.Sno}`}
-          >
-            Explore
-          </Link>
-        </CardBody>
-      </Card>
-      {/* </Col> */}
-    </>
+    <Card className="card-lift--hover shadow border-0 card-border product-thumbnail">
+      <CardBody className="py-3">
+        <ImageLoad
+          imageName={product.productImage}
+          altTxt={product.impurityName}
+        />
+        <h6 className="text-primary text-uppercase">{product.parentAPI}</h6>
+        <p className="description mt-3">{product.impurityName}</p>
+        <div>
+          <Badge color={thumbnailColor} pill className="mr-1">
+            {product.casNo}
+          </Badge>
+          <Badge color={thumbnailColor} pill className="mr-1">
+            {product.leadTime}
+          </Badge>
+          <Badge color={thumbnailColor} pill className="mr-1">
+            {product.readyStock}
+          </Badge>
+        </div>
+        <Link
+          type="button"
+          className="mt-4 btn btn-primary"
+          color="primary"
+          as={Link}
+          outline
+          to={`/productDetails/${product.Sno}`}
+        >
+          Explore
+        </Link>
+      </CardBody>
+    </Card>
   );
 });
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -10,6 +10,7 @@ import {
   Input,
   Badge,
 } from "reactstrap";
+import { FixedSizeGrid as Grid } from "react-window";
 import "./Products.scss";
 import useGlobalStore from "./../../store/global";
 
@@ -24,8 +25,6 @@ export const Products = () => {
   const updateProductsCategory = useGlobalStore(
     (state) => state.updateProductsCategory
   );
-  console.log("productsCategory", productsCategory);
-  console.log("selectedLetter", selectedLetter);
 
   useEffect(() => {
     filterProducts();
@@ -37,47 +36,38 @@ export const Products = () => {
 
   const handleLetterClick = (letter) => {
     setSelectedLetters((prevSelectedLetters) => {
-      // Check if the letter is already in the selected letters
       const newSelectedLetters = prevSelectedLetters.includes(letter)
         ? prevSelectedLetters.filter((l) => l !== letter)
         : [...prevSelectedLetters, letter];
 
-      // Remove empty string if there are other letters selected
       if (newSelectedLetters.includes("") && newSelectedLetters.length > 1) {
         return newSelectedLetters.filter((l) => l !== "");
       }
 
-      // Return the updated selected letters
       return newSelectedLetters;
     });
   };
 
-  // Filter the products
   const filterProducts = () => {
-    let filteredProductsList = products.slice(0, 25);
-    // Filter By Category
+    let filteredProductsList = products.slice(0, 1000); // Fetch more products for better demo
     if (productsCategory) {
       filteredProductsList = filteredProductsList.filter(
         (obj) => obj.category === productsCategory
       );
     }
-    // Filter By Selected Letters
     if (selectedLetters.length > 0) {
       filteredProductsList = filterObjectsByCharacters(
         filteredProductsList,
         selectedLetters
       );
     }
-    // Filter by searched text
     if (searchText) {
       filteredProductsList = filteredProductsList.filter((obj) => {
         const allProperties = JSON.stringify(obj).toLowerCase();
         return allProperties.includes(searchText.toLowerCase());
       });
-      console.log("searchText", searchText);
     }
 
-    console.log("filteredProductsList", filteredProductsList);
     setFilteredProducts(filteredProductsList);
   };
 
@@ -90,13 +80,9 @@ export const Products = () => {
         "productStatus",
         "category",
       ];
-
-      // Combine values of specified properties into a single string
       const combinedString = propertiesToCheck
         .map((prop) => obj[prop] || "")
         .join(" ");
-
-      // Check if any character in the character array is present in the combined string
       return characters.some((char) => combinedString.includes(char));
     });
   }
@@ -111,8 +97,23 @@ export const Products = () => {
 
   const searchProducts = (e) => {
     setSearchText(e.target.value);
-    console.log("dasdas", searchText);
-  }
+  };
+
+  const Cell = ({ columnIndex, rowIndex, style }) => {
+    const index = rowIndex * 3 + columnIndex;
+    if (index >= filteredProducts.length) return null;
+    const product = filteredProducts[index];
+
+    return (
+      <div style={style} className="d-flex justify-content-center">
+        <Product
+          product={product}
+          thumbnailColor={thumbnailsColors[index % thumbnailsColors.length]}
+          key={product.Sno}
+        />
+      </div>
+    );
+  };
 
   return (
     <>
@@ -171,7 +172,6 @@ export const Products = () => {
                   placeholder="Search Products"
                   onChange={searchProducts}
                   type="text"
-                  
                 />
               </FormGroup>
             </Col>
@@ -192,17 +192,23 @@ export const Products = () => {
                 )}
               </h5>
             </Col>
-            {/* <Col lg="5">
-              <h5 className="mb-0 text-right">
-                Products Count: &nbsp; {filteredProducts.length}
-              </h5>
-            </Col> */}
           </Row>
         </Container>
       </section>
       <Container>
         <Row className="justify-content-center">
           <Col lg="12">
+            {/* <Grid
+              columnCount={3}
+              columnWidth={400}
+              height={800}
+              rowCount={Math.ceil(filteredProducts.length / 3)}
+              rowHeight={500}
+              width={1200}
+            >
+              {Cell}
+            </Grid> */}
+
             <div className="grid-items-equal-height">
               {filteredProducts.map((product, index) => (
                 <Product
